ac37a90b8e7397715018dab8a708323093d7ce7f
santosh <santoshkumar95432@gmail.com>
2021-12-29 15:13:50 +0530

    blog post

--- src/app/pages/home/components/blogComments/comments/index.js
+++ src/app/pages/home/components/blogComments/comments/index.js
@@ -55,7 +55,7 @@ const Comments = ({ commentsUrl, currentUserId, user }) => {
     <div className='comments'>
       <h3 className='comments-title'>Comments</h3>
       <div className='comment-form-title'>Write comment</div>
-      <CommentForm submitLabel='Write' />
+      <CommentForm submitLabel='Comment' />
       <div className='comments-container'>
         {rootComments.map((rootComment) => (
           <Comment
