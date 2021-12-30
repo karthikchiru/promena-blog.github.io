9729db9331d4f400894949bf1e68d0ddae9eb0ee
santosh <santoshkumar95432@gmail.com>
2021-12-30 10:26:41 +0530

    post design changed

--- src/app/pages/home/staticpages/sidebar/index.js
+++ src/app/pages/home/staticpages/sidebar/index.js
@@ -3,61 +3,55 @@
 import React, { useState, useEffect } from 'react';
 import './index.scss'
 import { NavLink } from 'react-router-dom';
-import {getPostList} from '../../../../utils/apiCalls';
+import { getPostList } from '../../../../utils/apiCalls';
 import moment from 'moment';
 import ReadMore from '../../components/readmore';
 
 
 // eslint-disable-next-line no-unused-vars
-const Sidebar  = (props) => {
+const Sidebar = (props) => {
 
   const [postList, setPostList] = useState([]);
 
-  useEffect(()=>{
-    fetchData();
-  }, []);
+  useEffect(() => {
 
-  const fetchData = () =>{
-    getPostList((response)=>{
+
+    getPostList((response) => {
       console.log(response);
       setPostList(response);
     })
-  }
-  return(
+  }, []);
+
+  return (
 
     <div className='main1'>
 
-{ postList.length &&
-  postList.map(post => {
+      {
+        postList.map(post => {
+          var url = post.thumbnail;
+          // var pathname = new URL(url).pathname;
 
-    return(
-      <div className='posts' key={post.Blog_id}>
-      <img src={post.thumbnail} key={post.thumbnail} className='posts__image' />
-      <div className='posts__maincontent'>
-      <h3 className='posts__lead2'>
-          <NavLink  className = 'posts__link' to={`/post/${post.Blog_id}`}>  
-         {post.title}
-         </NavLink>
-          </h3>
-      
-          <div className='posts__content'>
-            <h1>{post.category}</h1>
-            <ReadMore post={post.content}/>
-       
-          </div>
-          <ul className='posts__ul'>
-          <li><i className='fa fa-clock-o' aria-hidden='true'> {moment(post.date_created).format('ddd-mm-yyyy, A')}</i></li>
-          <li><i className='fa fa-eye' aria-hidden='true'></i> 75 reads</li>
-          <li><i className='fa fa-clock-o' aria-hidden='true'></i> 3 min read</li>
-        </ul>
-</div>
-
-</div>
-)
-})
-}
-</div>
-)
+          return (
+            <div className='posts' key={post.Blog_id}>
+              <NavLink className='posts__link' to={`/post/${post.Blog_id}`}>
+                {post.title}
+              </NavLink>
+              <h1>{post.category}</h1>
+              <div className='posts__maincontent'>
+                <img src={url} key={post.thumbnail} className='posts__image' />
+                <ReadMore post={post.content} />
+              </div>
+              <ul className='posts__ul'>
+                <li><i className='fa fa-clock-o' aria-hidden='true'> {moment(post.date_created).format('ddd-mm-yyyy, A')}</i></li>
+                <li><i className='fa fa-eye' aria-hidden='true'></i> 75 reads</li>
+                <li><i className='fa fa-clock-o' aria-hidden='true'></i> 3 min read</li>
+              </ul>
+            </div>
+          )
+        })
+      }
+    </div>
+  )
 
 }
 
